845bea162a63df4d51f5baf97207727ed2dcd6a8
DNM1008 <dungnguyen10082000@gmail.com>
2025-01-22 20:40:10 +0700

    nvim config

--- .config/nvim/lua/zalmt/core/keymaps.lua
+++ .config/nvim/lua/zalmt/core/keymaps.lua
@@ -1,4 +1,4 @@
--- set leader key to space
+-- Set leader key to space
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
@@ -6,38 +6,35 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 -- General Keymaps -------------------
 
--- use jk to exit insert mode
--- keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
--- clear search highlights
+-- Clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
-
--- increment/decrement numbers
+-- Increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
--- window management
+-- Window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true }) -- make split window narrower
-keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true }) -- make split window wider
-keymap.set("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true }) -- make split window taller
-keymap.set("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true }) -- make split window shorter
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
+-- Resizing splits with Alt key
+keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+keymap.set("n", "<A-Up>", ":resize +2<CR>", { noremap = true, silent = true })
+keymap.set("n", "<A-Down>", ":resize -2<CR>", { noremap = true, silent = true })
+
+-- Tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>tl", ":tabs<CR>", { desc = "List all tabs" }) -- List all tabs
 
--- terminal split toggle
-local term_buf = nil
-local term_win = nil
+-----------------------
+-- Terminal Toggle Function -------------------
 
 function TermToggle(height)
 	if term_win and vim.api.nvim_win_is_valid(term_win) then
@@ -60,12 +57,14 @@ function TermToggle(height)
 		term_win = vim.api.nvim_get_current_win()
 	end
 end
-
--- term toggle keymaps
-keymap.set("n", "<leader><CR>", ":lua TermToggle(20)<CR>", { noremap = true, silent = true })
+-- Terminal keymaps
+keymap.set("n", "<leader><CR>", function()
+	TermToggle(20)
+end, { noremap = true, silent = true })
 keymap.set("i", "<leader><CR>", "<Esc>:lua TermToggle(20)<CR>", { noremap = true, silent = true })
 keymap.set("t", "<leader><CR>", "<C-\\><C-n>:lua TermToggle(20)<CR>", { noremap = true, silent = true })
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 
--- improve vim keys (?)
+-- Improve movement with wrapped lines
 keymap.set("n", "j", "gj", { desc = "Move down visually" })
 keymap.set("n", "k", "gk", { desc = "Move up visually" })
